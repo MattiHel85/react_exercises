3e6efd14941151e20fcf6ec6cfa2c70c3d2ee598
MattiHel85 <70198997+MattiHel85@users.noreply.github.com>
2021-01-24 13:28:30 +0200

    add Pokegame Component and functionality

--- pokedex-project/src/App.js
+++ pokedex-project/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import Pokedex from './Pokedex';
+// import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
+        {/* <Pokedex /> */}
+        <Pokegame />
       </div>
     );
   }
